4987818db1e0674444c947538d6ce2a6142415c7
Michael Martinez <soporte@mi-martinez.com>
2014-12-12 19:04:56 -0500

    Analitics y favicon

--- wp-content/themes/cb-modello/header.php
+++ wp-content/themes/cb-modello/header.php
@@ -66,24 +66,17 @@ $cb_type = esc_attr(get_post_meta($post->ID, '_cb5_post_type', 'true'));
         <?php
         wp_head();
         ?>
-        <link rel="shortcut icon" href="http://lilipink.com/wp-content/uploads/2014/10/favicon.ico">
-        <script>
-            (function (i, s, o, g, r, a, m) {
-                i['GoogleAnalyticsObject'] = r;
-                i[r] = i[r] || function () {
-                    (i[r].q = i[r].q || []).push(arguments)
-                }, i[r].l = 1 * new Date();
-                a = s.createElement(o),
-                    m = s.getElementsByTagName(o)[0];
-                a.async = 1;
-                a.src = g;
-                m.parentNode.insertBefore(a, m)
-            })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+        <link rel="shortcut icon" href="http://lilipinkintimasecret.com/wp-content/uploads/2014/12/favicon.ico">
+            <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-            ga('create', 'UA-52560577-11', 'auto');
-            ga('send', 'pageview');
+  ga('create', 'UA-52560577-13', 'auto');
+  ga('send', 'pageview');
 
-        </script>
+</script>
     </head>
 <?php $war = '';
 if (isset($cb_type)) {
